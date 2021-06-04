04c7dd48ba538148abeefd6c1586dcf78971b0cd
vaishgvs <84565228+vaishgvs@users.noreply.github.com>
2021-06-04 13:27:04 +0530

    sum test

--- functions/test.js
+++ functions/test.js
@@ -5,6 +5,7 @@
   })
 }*/
 
+/*
 exports.handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
 
@@ -16,5 +17,18 @@ exports.handler = async (event, context) => {
 // in the above function when I go to ...netlify.app/.netlify/functions/test?name=abc, it displays Hello, abc
 // else if I just go to ...netlify.app/.netlify/functions/test it displays Hello, World
 // so here name is the request parameter of the GET request
+*/
 
+//sum function 
 
+exports.handler = async (event, context) => {
+  const num1 = event.queryStringParameters.num1;
+  const num2 = event.queryStringParameters.num2;
+  
+  const sum = parseInt(num1) = parseInt(num2);
+  
+  return {
+    statusCode: 200,
+    body: 'Sum is: ${toString(sum)}',
+  };
+};
