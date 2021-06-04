2c7b1a9190ad89d9b4519b18cd7ae3e326362209
vaishgvs <84565228+vaishgvs@users.noreply.github.com>
2021-06-04 13:29:47 +0530

    = to +

--- functions/test.js
+++ functions/test.js
@@ -25,7 +25,7 @@ exports.handler = async (event, context) => {
   const num1 = event.queryStringParameters.num1;
   const num2 = event.queryStringParameters.num2;
   
-  const sum = parseInt(num1) = parseInt(num2);
+  const sum = parseInt(num1) + parseInt(num2);
   
   return {
     statusCode: 200,
